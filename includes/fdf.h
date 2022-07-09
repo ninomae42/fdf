17805ba1ca0e01cc04c61e6d1788b1b1d3afd598
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-07-09 15:33:54 +0900

    [Update] Add prototype declearation of utils.c #4

--- includes/fdf.h
+++ includes/fdf.h
@@ -13,6 +13,11 @@
 void	validate_argument(int argc, char *argv[]);
 int		open_file_by_name(const char *file_name);
 
+// utils.c
+char	**ft_split_or_exit(const char *s, char c);
+void	free_split(char **ptr);
+size_t	split_len(char **split);
+
 // hex_to_int.c
 int		hex_to_int(const char *hex);
 
