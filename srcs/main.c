474fb3a7c334269f227581e971534e723917a8d8
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-07-09 19:07:26 +0900

    [Add] Added window print logic.
    Added window print function that uses minilibx's API.

--- srcs/main.c
+++ srcs/main.c
@@ -1,14 +1,34 @@
 #include "../includes/fdf.h"
 
-int	main(int argc, char *argv[])
+typedef struct s_mlx
 {
-	t_map	*map_info;
+	void	*mlx;
+	void	*win;
+}	t_mlx;
 
-	map_info = init(argc, argv);
-	load_map(argv[1], map_info);
-	print_points(map_info);
-	exit(EXIT_SUCCESS);
+void	window_init(void)
+{
+	t_mlx	mlx;
+
+	mlx.mlx = mlx_init();
+	mlx.win = mlx_new_window(mlx.mlx, 700, 500, "fdf");
+	mlx_loop(mlx.mlx);
 }
+
+int	main(void)
+{
+	window_init();
+}
+
+//int	main(int argc, char *argv[])
+//{
+//	t_map	*map_info;
+//
+//	map_info = init(argc, argv);
+//	load_map(argv[1], map_info);
+//	print_points(map_info);
+//	exit(EXIT_SUCCESS);
+//}
 
 //__attribute__((destructor)) static void destructor()
 //{
