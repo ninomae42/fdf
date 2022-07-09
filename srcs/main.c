e5851bac3bc4574273a59d10dea9dd2f499b52e7
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-07-10 05:38:10 +0900

    [Update] Added scale calculation function #8
    Added scale calculation function to optimize image scale
    based on window size(height/width) and counts of dot.

--- srcs/main.c
+++ srcs/main.c
@@ -1,13 +1,5 @@
 #include "../includes/fdf.h"
 
-void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-	*(unsigned int *)dst = color;
-}
-
 int	calc_color(double l, double d, int color1, int color2)
 {
 	double	ret;
@@ -16,7 +8,20 @@ int	calc_color(double l, double d, int color1, int color2)
 	return (ret);
 }
 
-void	draw_line(t_img *img, t_point *p1, t_point *p2)
+double	calculate_scale(t_map *map_info)
+{
+	double	scale_x;
+	double	scale_y;
+
+	scale_x = 1000.0 / map_info->width;
+	scale_y = 600.0 / map_info->height;
+	if (scale_x < scale_y)
+		return (scale_x);
+	else
+		return (scale_y);
+}
+
+void	draw_line(t_img *img, double scale, t_point *p1, t_point *p2)
 {
 	double	dx;
 	double	dy;
@@ -24,34 +29,41 @@ void	draw_line(t_img *img, t_point *p1, t_point *p2)
 	double	d;
 	double	l;
 
-	dx = 50 * (p2->x - p1->x);
-	dy = 50 * (p2->y - p1->y);
+	dx = scale * (p2->x - p1->x);
+	dy = scale * (p2->y - p1->y);
 	theta = atan2(dy, dx);
 	d = sqrt(dx * dx + dy * dy);
 	l = 0;
 	while (l < d)
 	{
 		my_mlx_pixel_put(img,
-			p1->x * 50 + l * cos(theta),
-			p1->y * 50 + l * sin(theta),
+			p1->x * scale + l * cos(theta),
+			p1->y * scale + l * sin(theta),
 			0x00FFFFFF);
 		l++;
 	}
 }
 
-void	connect_other_points(t_img *img, t_map *map_info, size_t index_x, size_t index_y)
+void	connect_other_points(
+		t_img *img, t_map *map_info, size_t index_x, size_t index_y)
 {
 	t_point	**map;
+	double	scale;
 
 	map = map_info->map;
+	scale = calculate_scale(map_info);
 	if (index_y + 1 < map_info->height)
-		draw_line(img, &map[index_y][index_x], &map[index_y + 1][index_x]);
+		draw_line(img, scale, &map[index_y][index_x],
+			&map[index_y + 1][index_x]);
 	if (index_x + 1 < map_info->width)
-		draw_line(img, &map[index_y][index_x], &map[index_y][index_x + 1]);
+		draw_line(img, scale, &map[index_y][index_x],
+			&map[index_y][index_x + 1]);
 	if (0 < index_y)
-		draw_line(img, &map[index_y - 1][index_x], &map[index_y][index_x]);
+		draw_line(img, scale, &map[index_y - 1][index_x],
+			&map[index_y][index_x]);
 	if (0 < index_x)
-		draw_line(img, &map[index_y][index_x - 1], &map[index_y][index_x]);
+		draw_line(img, scale, &map[index_y][index_x - 1],
+			&map[index_y][index_x]);
 }
 
 void	print_grid(t_map *map_info, t_mlx *mlx, t_img *img)
@@ -74,36 +86,33 @@ void	print_grid(t_map *map_info, t_mlx *mlx, t_img *img)
 	}
 }
 
-void	window_init(t_map *map_info)
+t_mlx	*init_mlx(void)
 {
-	t_mlx	mlx;
-	t_img	img;
-	t_point	p1;
-	t_point	p2;
+	t_mlx	*mlx;
 
-	mlx.mlx = mlx_init();
-	mlx.win = mlx_new_window(mlx.mlx, 1200, 800, "fdf");
-	mlx_hook(mlx.win, KEY_PRESS, 1L << 0, close_window_with_esc, &mlx);
-	mlx_hook(mlx.win, DESTROY_NOTIFY, 1L << 17, close_window_with_button, &mlx);
-
-	img.img = mlx_new_image(mlx.mlx, 1200, 800);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
-			&img.line_length, &img.endian);
-
-	print_grid(map_info, &mlx, &img);
-
-	mlx_put_image_to_window(mlx.mlx, mlx.win, img.img, 100, 100);
-	mlx_loop(mlx.mlx);
+	mlx = ft_malloc_or_exit(sizeof(t_mlx));
+	mlx->mlx = mlx_init();
+	mlx->win = mlx_new_window(mlx->mlx, 1200, 800, "fdf");
+	mlx_hook(mlx->win, KEY_PRESS, 1L << 0, close_window_with_esc, mlx);
+	mlx_hook(mlx->win, DESTROY_NOTIFY, 1L << 17, close_window_with_button, mlx);
+	return (mlx);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_map	*map_info;
+	t_mlx	*mlx;
+	t_img	img;
 
 	map_info = init(argc, argv);
 	load_map(argv[1], map_info);
-	//print_points(map_info);
-	window_init(map_info);
+	mlx = init_mlx();
+	img.img = mlx_new_image(mlx->mlx, 1000, 600);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+			&img.line_length, &img.endian);
+	print_grid(map_info, mlx, &img);
+	mlx_put_image_to_window(mlx->mlx, mlx->win, img.img, 100, 100);
+	mlx_loop(mlx->mlx);
 	exit(EXIT_SUCCESS);
 }
 
