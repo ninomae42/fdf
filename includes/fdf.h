db35ecc18d8f0920bb36f8bccc2955d87bc57d57
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2023-02-19 22:12:26 +0900

    必要なヘッダーファイルをインクルード

--- includes/fdf.h
+++ includes/fdf.h
@@ -1,6 +1,11 @@
 #ifndef FDF_H
 # define FDF_H
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
 
 #endif
