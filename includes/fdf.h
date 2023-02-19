3d33a19ab5b23e30924d2ebe4d7db3cc94813197
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2023-02-19 23:29:15 +0900

    mapの縦横の要素数を計算する関数を追加

--- includes/fdf.h
+++ includes/fdf.h
@@ -21,6 +21,8 @@ void	*ft_malloc(size_t size);
 // hex_to_int.c
 int		hex_to_int(const char *hex);
 
+// map_utils.c
+int		set_map_width_height(const char *file_name, t_map *map);
 // ft_split_utils.c
 char	**ft_split_safe(char *s, char c);
 size_t	ft_split_len(char **split);
---
+++ srcs/map_utils.c
@@ -0,0 +1,28 @@
+#include "fdf.h"
+
+int	set_map_width_height(const char *file_name, t_map *map)
+{
+	int		fd;
+	char	*row;
+	char	**points;
+
+	fd = open_file_by_name(file_name);
+	row = get_next_line(fd);
+	while (row != NULL)
+	{
+		points = ft_split_safe(row, ' ');
+		free(row);
+		if (map->cols == 0)
+			map->cols = ft_split_len(points);
+		else if (map->cols != ft_split_len(points))
+		{
+			ft_split_free(points);
+			return (1);
+		}
+		ft_split_free(points);
+		row = get_next_line(fd);
+		map->rows++;
+	}
+	close(fd);
+	return (0);
+}
