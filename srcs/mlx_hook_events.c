7b743d601056a9435dec2f15cfac9e25552b075a
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2023-02-21 18:25:30 +0900

    正常終了時にリソースを開放

--- srcs/mlx_hook_events.c
+++ srcs/mlx_hook_events.c
@@ -53,6 +53,8 @@ static void	handle_close(t_info *info)
 	mlx_loop_end(info->mlx->mlx_ptr);
 	deallocate_mlx(info->mlx);
 	deallocate_map(info->map);
+	deallocate_map(info->draw_map);
+	state_deallocate(info->state);
 	exit(EXIT_SUCCESS);
 }
 
